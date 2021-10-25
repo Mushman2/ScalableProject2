5ef3a1dae12f7d6cce85b448b07229e2b0cc850f
jackgilbride999 <jackgilbride999@gmail.com>
2021-10-25 16:13:00 +0100

    error handling

--- getFiles.py
+++ getFiles.py
@@ -2,4 +2,10 @@ import requests
 
 query = {'shortname':'gilbridj'}
 response = requests.get("https://www.cs7ns1.scss.tcd.ie", params=query)
-print(response)
+
+if response.status_code != 200:
+    print("error getting file")
+    print(response)
+    return 1
+
+print(response.content.decode('utf-8'))
