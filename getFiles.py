187aaa053513227d04df83b821df7a5613783bc2
jackgilbride999 <jackgilbride999@gmail.com>
2021-10-25 20:04:22 +0100

    print statements

--- getFiles.py
+++ getFiles.py
@@ -13,23 +13,27 @@ response = requests.get("https://cs7ns1.scss.tcd.ie/", params=query)
 
 with open(shortname, 'wb') as writeable:
     writeable.write(response.content)
+    print("Got initial response.")
 
 with open(shortname) as readable:
     content = readable.read()
     path = content.split("<a href='")[1]
     path = path.split("' >")[0]
-    print(path)
+    print("Parsed path from initial response.")
 
 response = requests.get("https://cs7ns1.scss.tcd.ie/" + path)
 with open(shortname + '.csv', 'wb') as writeable:
     writeable.write(response.content)
+    print("Got second response.")
 
 with open(shortname + '.csv') as readable:
     content = readable.read()
     splitContent = content.split(",")
+    print("Split second response.")
 
 if not os.path.exists("./" + shortname + "images"):
     os.makedirs("./" + shortname + "images")
+    print("Created images directory.")
 
 for filename in splitContent:
     filename = filename.strip()
@@ -38,4 +42,4 @@ for filename in splitContent:
         shutil.copyfileobj(response.raw, imageFile)
         del response
 
-
+print("Complete.")
