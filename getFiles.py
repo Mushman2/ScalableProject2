39787b1c48ead2c9ce781ca9d27d4b38f0e5d6ae
jackgilbride999 <jackgilbride999@gmail.com>
2021-10-25 16:13:42 +0100

    delete return

--- getFiles.py
+++ getFiles.py
@@ -6,6 +6,5 @@ response = requests.get("https://www.cs7ns1.scss.tcd.ie", params=query)
 if response.status_code != 200:
     print("error getting file")
     print(response)
-    return 1
 
 print(response.content.decode('utf-8'))
