35b4f5d153309949e7876980bd2ab970d69d9ec5
jackgilbride999 <jackgilbride999@gmail.com>
2021-10-25 20:39:14 +0100

    gitignore

--- getFiles.py
+++ getFiles.py
@@ -40,9 +40,8 @@ if not os.path.exists(dirName):
 for filename in splitContent:
     filename = filename.strip()
     response = requests.get("https://cs7ns1.scss.tcd.ie/"+filename, params=query, stream =True)
-    if(not(filename.isspace())):
-        with open(dirName + "/" + filename, 'wb') as imageFile:
-            shutil.copyfileobj(response.raw, imageFile)
-            del response
+    with open(dirName + "/" + filename, 'wb') as imageFile:
+        shutil.copyfileobj(response.raw, imageFile)
+        del response
 
 print("Complete.")
