43370d34083077abf64c7db8f0b909b201806797
jackgilbride999 <jackgilbride999@gmail.com>
2021-10-25 15:58:02 +0100

    use requests library

--- getFiles.py
+++ getFiles.py
@@ -1,7 +1,5 @@
-import wget
-# Define the remote file to retrieve
-remote_url = 'http://www.cs7ns1.scss.tcd.ie/gilbridj'
-# Define the local filename to save data
-local_file = 'gilbridj'
-# Make http request for remote file data
-wget.download(remote_url, local_file)
+import requests
+
+query = {'shortname':'gilbridj'}
+response = requests.get("http://www.cs7ns1.scss.tcd.ie")
+print(response)
