0f863a18c0d6ff2dbc72df3b9ae34951217026de
Jack Gilbride <jackgilbride999@gmail.com>
2021-10-25 20:40:41 +0100

    Add Python Script to pull files (#1)
    * first attempt at script
    
    * use requests library
    
    * https
    
    * params=query
    
    * error handling
    
    * delete return
    
    * forward slash
    
    * remove www
    
    * save file
    
    * r->reponse
    
    * read file
    
    * caps lock
    
    * parse line
    
    * array index
    
    * redo splitting
    
    * push
    
    * push
    
    * print whole file
    
    * decode
    
    * read file
    
    * shortname
    
    * temp
    
    * temp
    
    * test link extraction
    
    * print second file
    
    * comment
    
    * split file
    
    * try save images
    
    * mkdir
    
    * fix typo
    
    * image
    
    * command line args
    
    * +
    
    * len
    
    * sys
    
    * sys
    
    * 2
    
    * print statements
    
    * wrong argument
    
    * dirname
    
    * _
    
    * error handling
    
    * error handling
    
    * error handling
    
    * gitignore

---
+++ getFiles.py
@@ -0,0 +1,47 @@
+import requests
+import os
+import shutil
+import sys
+
+if(len(sys.argv) != 2):
+    print("Script should be passed one argument, your network username.")
+
+shortname = sys.argv[1]
+
+query = {'shortname': shortname}
+response = requests.get("https://cs7ns1.scss.tcd.ie/", params=query)
+
+with open(shortname, 'wb') as writeable:
+    writeable.write(response.content)
+    print("Got initial response.")
+
+with open(shortname) as readable:
+    content = readable.read()
+    path = content.split("<a href='")[1]
+    path = path.split("' >")[0]
+    print("Parsed path from initial response.")
+
+response = requests.get("https://cs7ns1.scss.tcd.ie/" + path)
+with open(shortname + '.csv', 'wb') as writeable:
+    writeable.write(response.content)
+    print("Got second response.")
+
+with open(shortname + '.csv') as readable:
+    content = readable.read()
+    splitContent = content.split(",")
+    print("Split second response.")
+
+dirName = "./" + shortname + "_images"
+
+if not os.path.exists(dirName):
+    os.makedirs(dirName)
+    print("Created images directory.")
+
+for filename in splitContent:
+    filename = filename.strip()
+    response = requests.get("https://cs7ns1.scss.tcd.ie/"+filename, params=query, stream =True)
+    with open(dirName + "/" + filename, 'wb') as imageFile:
+        shutil.copyfileobj(response.raw, imageFile)
+        del response
+
+print("Complete.")
