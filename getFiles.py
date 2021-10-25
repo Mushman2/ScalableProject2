b40c4b05c5faa7c30b1b6e605b9fb96de3983bf7
jackgilbride999 <jackgilbride999@gmail.com>
2021-10-25 16:42:28 +0100

    parse line

--- getFiles.py
+++ getFiles.py
@@ -8,4 +8,6 @@ with open('gilbridj', 'wb') as writeable:
 
 with open('gilbridj') as readable:
     first_line = readable.readline()
+    new_request = first_line.split(": ")
+    new_request = new_request.split(": ")
     print(first_line)
