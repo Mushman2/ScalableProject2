af9b90b6be976db20e6e59adcd4de3d80da23ebc
jackgilbride999 <jackgilbride999@gmail.com>
2021-10-25 16:48:47 +0100

    push

--- getFiles.py
+++ getFiles.py
@@ -10,3 +10,13 @@ with open('gilbridj') as readable:
     first_line = readable.readline()
     new_request = first_line.split(": ")[2]
     print(new_request)
+
+
+response = requests.get("https://cs7ns1.scss.tcd.ie/" + new_request, params=query)
+with open('gilbridj-challenge-filenames.csv', 'wb') as writeable:
+    writeable.write(response.content)
+
+with open('gilbridj-challenge-filenames.csv') as readable:
+    first_line = readable.readline()
+    new_request = first_line.split(": ")[2]
+    print(new_request)
