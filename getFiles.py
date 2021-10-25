5d1c35a58d94eed12dc4bd92c203e3aa7a7d0453
jackgilbride999 <jackgilbride999@gmail.com>
2021-10-25 17:36:30 +0100

    print second file

--- getFiles.py
+++ getFiles.py
@@ -8,8 +8,16 @@ with open('gilbridj', 'wb') as writeable:
 
 with open('gilbridj') as readable:
     content = readable.read()
-    content = content.split("<a href='")[1]
-    content = content.split("' >")[0]
+    path = content.split("<a href='")[1]
+    path = path.split("' >")[0]
+    print(path)
+
+response = requests.get("https://cs7ns1.scss.tcd.ie/" + path)
+with open('gilbridj.csv', 'wb') as writeable:
+    writeable.write(response.content)
+
+with open('gilbridj.csv') as readable:
+    content = readable.read()
     print(content)
 
 '''
