b0c262b76dd8e3959b6cd66d711f5ec67e76482f
Mushman2 <hartigam@tcd.ie>
2021-10-27 16:55:06 +0100

    implemented padding for generate and train

--- generate.py
+++ generate.py
@@ -10,25 +10,18 @@ import captcha.image
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--width', help='Width of captcha image', type=int)
-    parser.add_argument('--height', help='Height of captcha image', type=int)
-    parser.add_argument('--length', help='Length of captchas in characters', type=int)
+
+    captchaWidth = 128
+    captchaHeight = 64
+    captchaMinLength = 1
+    captchaMaxLength = 6
+    symbolsDir = "symbols.txt"
+
     parser.add_argument('--count', help='How many captchas to generate', type=int)
     parser.add_argument('--output-dir', help='Where to store the generated captchas', type=str)
-    parser.add_argument('--symbols', help='File with the symbols to use in captchas', type=str)
     args = parser.parse_args()
 
-    if args.width is None:
-        print("Please specify the captcha image width")
-        exit(1)
 
-    if args.height is None:
-        print("Please specify the captcha image height")
-        exit(1)
-
-    if args.length is None:
-        print("Please specify the captcha length")
-        exit(1)
 
     if args.count is None:
         print("Please specify the captcha count to generate")
@@ -38,13 +31,9 @@ def main():
         print("Please specify the captcha output directory")
         exit(1)
 
-    if args.symbols is None:
-        print("Please specify the captcha symbols file")
-        exit(1)
+    captcha_generator = captcha.image.ImageCaptcha(width=captchaWidth, height=captchaHeight)
 
-    captcha_generator = captcha.image.ImageCaptcha(width=args.width, height=args.height)
-
-    symbols_file = open(args.symbols, 'r')
+    symbols_file = open(symbolsDir, 'r')
     captcha_symbols = symbols_file.readline().strip()
     symbols_file.close()
 
@@ -55,14 +44,14 @@ def main():
         os.makedirs(args.output_dir)
 
     for i in range(args.count):
-        random_str = ''.join([random.choice(captcha_symbols) for j in range(args.length)])
+        random_str = ''.join([random.choice(captcha_symbols) for j in range(random.randint(captchaMinLength, captchaMaxLength))])
         image_path = os.path.join(args.output_dir, random_str+'.png')
         if os.path.exists(image_path):
             version = 1
             while os.path.exists(os.path.join(args.output_dir, random_str + '_' + str(version) + '.png')):
                 version += 1
             image_path = os.path.join(args.output_dir, random_str + '_' + str(version) + '.png')
-
+        image_path = image_path.replace(":","!")
         image = numpy.array(captcha_generator.generate_image(random_str))
         cv2.imwrite(image_path, image)
 
--- train.py
+++ train.py
@@ -52,13 +52,12 @@ class ImageSequence(keras.utils.Sequence):
 
     def __getitem__(self, idx):
         X = numpy.zeros((self.batch_size, self.captcha_height, self.captcha_width, 3), dtype=numpy.float32)
-        y = [numpy.zeros((self.batch_size, len(self.captcha_symbols)), dtype=numpy.uint8) for i in range(self.captcha_length)]
+        y = [numpy.zeros((self.batch_size, len(self.captcha_symbols)+1), dtype=numpy.uint8) for i in range(self.captcha_length)]
 
         for i in range(self.batch_size):
 
             image_label = list(self.files.keys())[idx*self.batch_size+i]
             image_file = self.files[image_label]
-
             raw_data = cv2.imread(os.path.join(self.directory_name, image_file))
             rgb_data = cv2.cvtColor(raw_data, cv2.COLOR_BGR2RGB)
             processed_data = numpy.array(rgb_data) / 255.0
@@ -68,42 +67,34 @@ class ImageSequence(keras.utils.Sequence):
             # So the real label should have the "_num" stripped out.
 
             image_label = image_label.split('_')[0]
+            while len(image_label) < 6:
+                image_label = image_label + " "
 
             for j, ch in enumerate(image_label):
                 y[j][i, :] = 0
-                y[j][i, self.captcha_symbols.find(ch)] = 1
+                if ch == "!": ch = ':'
+                if ch == " ":
+                    y[j][i, len(self.captcha_symbols)] = 1
+                else:
+                    y[j][i, self.captcha_symbols.find(ch)] = 1
 
         return X, y
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--width', help='Width of captcha image', type=int)
-    parser.add_argument('--height', help='Height of captcha image', type=int)
-    parser.add_argument('--length', help='Length of captchas in characters', type=int)
-    parser.add_argument('--batch-size', help='How many images in training captcha batches', type=int)
+    captchaWidth = 128
+    captchaHeight = 64
+    captchaMinLength = 1
+    captchaMaxLength = 6
+    symbolsDir = "symbols.txt"
+    batchSize = 32
+
     parser.add_argument('--train-dataset', help='Where to look for the training image dataset', type=str)
     parser.add_argument('--validate-dataset', help='Where to look for the validation image dataset', type=str)
     parser.add_argument('--output-model-name', help='Where to save the trained model', type=str)
     parser.add_argument('--input-model', help='Where to look for the input model to continue training', type=str)
     parser.add_argument('--epochs', help='How many training epochs to run', type=int)
-    parser.add_argument('--symbols', help='File with the symbols to use in captchas', type=str)
     args = parser.parse_args()
-
-    if args.width is None:
-        print("Please specify the captcha image width")
-        exit(1)
-
-    if args.height is None:
-        print("Please specify the captcha image height")
-        exit(1)
-
-    if args.length is None:
-        print("Please specify the captcha length")
-        exit(1)
-
-    if args.batch_size is None:
-        print("Please specify the training batch size")
-        exit(1)
 
     if args.epochs is None:
         print("Please specify the number of training epochs to run")
@@ -121,12 +112,8 @@ def main():
         print("Please specify a name for the trained model")
         exit(1)
 
-    if args.symbols is None:
-        print("Please specify the captcha symbols file")
-        exit(1)
-
     captcha_symbols = None
-    with open(args.symbols) as symbols_file:
+    with open(symbolsDir) as symbols_file:
         captcha_symbols = symbols_file.readline()
 
     # physical_devices = tf.config.experimental.list_physical_devices('GPU')
@@ -136,7 +123,7 @@ def main():
     with tf.device('/device:GPU:0'):
     # with tf.device('/device:CPU:0'):
     # with tf.device('/device:XLA_CPU:0'):
-        model = create_model(args.length, len(captcha_symbols), (args.height, args.width, 3))
+        model = create_model(captchaMaxLength, len(captcha_symbols) + 1, (captchaHeight, captchaWidth, 3))
 
         if args.input_model is not None:
             model.load_weights(args.input_model)
@@ -147,8 +134,8 @@ def main():
 
         model.summary()
 
-        training_data = ImageSequence(args.train_dataset, args.batch_size, args.length, captcha_symbols, args.width, args.height)
-        validation_data = ImageSequence(args.validate_dataset, args.batch_size, args.length, captcha_symbols, args.width, args.height)
+        training_data = ImageSequence(args.train_dataset, batchSize, captchaMaxLength, captcha_symbols, captchaWidth, captchaHeight)
+        validation_data = ImageSequence(args.validate_dataset, batchSize, captchaMaxLength, captcha_symbols, captchaWidth, captchaHeight)
 
         callbacks = [#keras.callbacks.EarlyStopping(patience=3),
                      # keras.callbacks.CSVLogger('log.csv'),
@@ -163,11 +150,18 @@ def main():
                                 validation_data=validation_data,
                                 epochs=args.epochs,
                                 callbacks=callbacks,
-                                use_multiprocessing=True,
-                                steps_per_epoch = 200)
+                                use_multiprocessing=True)
         except KeyboardInterrupt:
             print('KeyboardInterrupt caught, saving current weights as ' + args.output_model_name+'_resume.h5')
             model.save_weights(args.output_model_name+'_resume.h5')
+
+        # Convert the model.
+        converter = tf.lite.TFLiteConverter.from_keras_model(model)
+        tflite_model = converter.convert()
+
+        # Save the model.
+        with open(args.output_model_name+'.tflite', 'wb') as f:
+            f.write(tflite_model)
 
 if __name__ == '__main__':
     main()
