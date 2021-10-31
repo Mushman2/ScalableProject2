0f7a5b7c2cc618f062fb879b7fb240e27658dd0a
jackgilbride999 <jackgilbride999@gmail.com>
2021-10-31 12:19:58 +0000

    update getFiles

--- getFiles.py
+++ getFiles.py
@@ -3,61 +3,72 @@ import os
 import shutil
 import sys
 
-if(len(sys.argv) != 2):
-    print("Script should be passed one argument, your network username.")
 
-shortname = sys.argv[1]
+def main():
+        
+    if(len(sys.argv) != 2):
+        print("Script should be passed one argument, your network username.")
 
-query = {'shortname': shortname}
-response = requests.get("https://cs7ns1.scss.tcd.ie/", params=query)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--username', help='TCD Username', type=str)
+    args = parser.parse_args()
 
-with open(shortname, 'wb') as writeable:
-    writeable.write(response.content)
-    print("Got initial response.")
+    if args.username is None:
+        print("Please specify your TCD Username")
+        exit(1)
 
-with open(shortname) as readable:
-    content = readable.read()
-    path = content.split("<a href='")[1]
-    path = path.split("' >")[0]
-    print("Parsed path from initial response.")
+    shortname = args.username
 
-response = requests.get("https://cs7ns1.scss.tcd.ie/" + path)
-with open(shortname + '.csv', 'wb') as writeable:
-    writeable.write(response.content)
-    print("Got second response.")
+    query = {'shortname': shortname}
+    response = requests.get("https://cs7ns1.scss.tcd.ie/", params=query)
 
-with open(shortname + '.csv') as readable:
-    content = readable.read()
-    splitContent = content.split(",")
-    print("Split second response.")
-
-htmlDirName = "./" + shortname + "_htmls"
-if not os.path.exists(htmlDirName):
-    os.makedirs(htmlDirName)
-    print("Created htmls directory.")
-
-imageDirName = "./" + shortname + "_images"
-if not os.path.exists(imageDirName):
-    os.makedirs(imageDirName)
-    print("Created images directory.")
-
-for filename in splitContent:
-    filename = filename.strip()
-    query = {'shortname': shortname, 'myfilename': filename}
-    response = requests.get("https://cs7ns1.scss.tcd.ie/", params=query, stream=True) 
-    with open(htmlDirName + "/" + filename + ".html", 'wb') as writeable:
+    with open(shortname, 'wb') as writeable:
         writeable.write(response.content)
+        print("Got initial response.")
 
-    with open(htmlDirName + "/" + filename + ".html") as readable:
+    with open(shortname) as readable:
         content = readable.read()
         path = content.split("<a href='")[1]
         path = path.split("' >")[0]
+        print("Parsed path from initial response.")
 
-    response = requests.get("https://cs7ns1.scss.tcd.ie/" + path, stream=True)
+    response = requests.get("https://cs7ns1.scss.tcd.ie/" + path)
+    with open(shortname + '.csv', 'wb') as writeable:
+        writeable.write(response.content)
+        print("Got second response.")
 
-    with open(imageDirName + "/" + filename, 'wb') as imageFile:
-        response.raw.decode_content = True
-        shutil.copyfileobj(response.raw, imageFile)
-        del response
+    with open(shortname + '.csv') as readable:
+        content = readable.read()
+        splitContent = content.split(",")
+        print("Split second response.")
 
-print("Complete.")
+    htmlDirName = "./" + shortname + "_htmls"
+    if not os.path.exists(htmlDirName):
+        os.makedirs(htmlDirName)
+        print("Created htmls directory.")
+
+    imageDirName = "./" + shortname + "_images"
+    if not os.path.exists(imageDirName):
+        os.makedirs(imageDirName)
+        print("Created images directory.")
+
+    for filename in splitContent:
+        filename = filename.strip()
+        query = {'shortname': shortname, 'myfilename': filename}
+        response = requests.get("https://cs7ns1.scss.tcd.ie/", params=query, stream=True) 
+        with open(htmlDirName + "/" + filename + ".html", 'wb') as writeable:
+            writeable.write(response.content)
+
+        with open(htmlDirName + "/" + filename + ".html") as readable:
+            content = readable.read()
+            path = content.split("<a href='")[1]
+            path = path.split("' >")[0]
+
+        response = requests.get("https://cs7ns1.scss.tcd.ie/" + path, stream=True)
+
+        with open(imageDirName + "/" + filename, 'wb') as imageFile:
+            response.raw.decode_content = True
+            shutil.copyfileobj(response.raw, imageFile)
+            del response
+
+    print("Complete.")
